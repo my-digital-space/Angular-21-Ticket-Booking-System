e75437f810e888ee7bd9ff550ffe0d464f89c519
prosenjit005 <mydigitalspace.pro@gmail.com>
2026-02-06 20:55:34 +0530

    basic question setup is done

--- ticket-booking-system/src/app/app.ts
+++ ticket-booking-system/src/app/app.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // CommonModule: ✅ enables *ngFor, *ngIf, pipes etc.
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ticket-booking-system');
+
+  sections = [0, 1, 2];
+
+  seatsPerSection = 18;
+
+  // 1 → 18 (base seats per section)
+  seatArray: number[] = Array.from({ length: this.seatsPerSection }, (_, i) => i + 1);
 }
