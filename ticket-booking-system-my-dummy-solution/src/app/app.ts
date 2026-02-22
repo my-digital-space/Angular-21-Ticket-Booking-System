e7f0ef71e5f0294159f6493221bed41a62c08e96
prosenjit005 <mydigitalspace.pro@gmail.com>
2026-02-22 19:32:38 +0530

    completed my dummy solution

--- ticket-booking-system-my-dummy-solution/src/app/app.ts
+++ ticket-booking-system-my-dummy-solution/src/app/app.ts
@@ -1,12 +1,51 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ticket-booking-system');
+  seatsList: number[] = Array.from({ length: 20 }, (_, i) => i + 1);
+
+  selectedSeats: Set<number> = new Set<number>();
+  bookedSeats: Set<number> = new Set<number>();
+
+  trackBySeat(_: number, value: number) {
+    return value;
+  }
+
+  resetSeats() {
+    this.selectedSeats.clear();
+  }
+
+  toggleSeat(seatNumber: number) {
+    // If seat already booked → do nothing
+    if (this.bookedSeats.has(seatNumber)) {
+      return;
+    }
+
+    // If already selected → deselect
+    if (this.selectedSeats.has(seatNumber)) {
+      this.selectedSeats.delete(seatNumber);
+    }
+    // Else select
+    else {
+      this.selectedSeats.add(seatNumber);
+    }
+  }
+
+  bookSeats() {
+    // Move selected seats to booked
+    this.selectedSeats.forEach(seat => {
+      this.bookedSeats.add(seat);
+    });
+
+    // Clear selected after booking
+    this.selectedSeats.clear();
+  }
+
 }
